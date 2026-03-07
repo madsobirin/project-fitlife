fd501735b9d94da287bb3ce42ea955205ed34ef3
madSobirin <gamingbrn202@gmail.com>
2026-03-08 06:45:13 +0700

    membuat duplikasi page login

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,12 +1,10 @@
 "use client";
 
 import { useState } from "react";
-import { useFormStatus } from "react-dom";
 import { login } from "@/actions/auth";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Button } from "@/components/ui/button";
-import { Loader2, Eye } from "lucide-react";
+import { Eye } from "lucide-react";
 import Image from "next/image";
 import { motion, Variants } from "framer-motion";
 import AuthLayout from "@/components/auth/authLayout";
@@ -33,24 +31,6 @@ const item: Variants = {
     },
   },
 };
-
-// function SubmitButton() {
-//   const { pending } = useFormStatus();
-
-//   return (
-//     <Button
-//       type="submit"
-//       disabled={pending}
-//       className="w-full flex items-center justify-center gap-2 rounded-2xl bg-emerald-500 py-[26px] font-semibold text-white hover:bg-emerald-600 active:scale-[0.97] transition shadow-sm"
-//     >
-//       {pending ? (
-//         <Loader2 className="animate-spin h-5 w-5" />
-//       ) : (
-//         <>Continue account →</>
-//       )}
-//     </Button>
-//   );
-// }
 
 export default function LoginPage() {
   const [formData, setFormData] = useState({
