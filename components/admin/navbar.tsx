7ac83b08f1f55d76ce3d79c0e42d948181917a8e
madSobirin <gamingbrn202@gmail.com>
2026-03-13 10:52:40 +0700

    menghapus icon lonceng dii page admin

--- components/admin/navbar.tsx
+++ components/admin/navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Search, Bell, Menu, Sun, Moon } from "lucide-react";
+import { Search, Menu, Sun, Moon } from "lucide-react";
 import Image from "next/image";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
