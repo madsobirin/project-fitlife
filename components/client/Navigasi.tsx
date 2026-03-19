accb5d4d5812d3a58385661aed3395e9151ef4b9
madSobirin <gamingbrn202@gmail.com>
2026-03-19 13:04:53 +0700

    membuat halaman kalkulator

--- components/client/Navigasi.tsx
+++ components/client/Navigasi.tsx
@@ -118,7 +118,7 @@ export default function Navbar() {
               Kalkulator BMI
             </Link>
             <Link
-              href="/menu-sehat"
+              href="/menu"
               className="block text-text-muted hover:text-primary font-medium transition-colors py-2"
               onClick={() => setMobileOpen(false)}
             >
